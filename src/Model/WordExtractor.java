33ab36f22abe6c11e0e7df885e2f25a26814754a
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2015-04-27 10:19:59 +0200

    finish class WordExtractor that take the documents list do the following \n - get all stopping words from files \n - eleminate stopping words

--- src/Model/WordExtractor.java
+++ src/Model/WordExtractor.java
@@ -1,10 +1,17 @@
 package Model;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordExtractor {
 	private ArrayList<Document> documents;
 	private ArrayList<String> stoppingWords;
 	
+	public WordExtractor() {
+		documents = new ArrayList<Document>();
+		stoppingWords = new ArrayList<String>();
+	}
 	public WordExtractor(ArrayList<Document> doc, ArrayList<String> stopping) {
 		documents = (ArrayList<Document>) doc;
 		stoppingWords = (ArrayList<String>) stopping;
@@ -31,9 +38,48 @@ public class WordExtractor {
 	public void addStoppingWords(ArrayList<String> words) {
 		stoppingWords = words;
 	}
+	public boolean addStoppingWords(File stopFile) {
+		if(stopFile.canRead()) {
+			try {
+				Scanner scan = new Scanner(stopFile);
+				while(scan.hasNext()) {
+					String line = scan.nextLine();
+					String [] words = line.split(" ");
+					for(String word : words) {
+						stoppingWords.add(word);
+					}
+				}
+				scan.close();
+				return true;
+			} catch (FileNotFoundException e) {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	private static ArrayList<File> getFiles(String folderName) {
+		File folder = new File(folderName);
+		ArrayList<File> files = new ArrayList<File>();
+		for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	        	getFiles(fileEntry.getName());
+	        } 
+	        else {
+	            files.add(fileEntry);
+	        }
+	    }
+		return files;
+	}
 	public void printDocument() {
 		for(Document doc :documents) {
 			System.out.println(doc);
+		}
+	}
+	public void printStoppingWords() {
+		for(String word : stoppingWords) {
+			System.out.println(word);
 		}
 	}
 	public static void simpleEleminateStopWordsTest() {
@@ -53,5 +99,13 @@ public class WordExtractor {
 		x.addDocument(doc);
 		x.elemenateStoppingWords();
 		x.printDocument();
+	}
+	public static void simpleExtractStoppingWordsFromFilesTest(String directoryName) {
+		WordExtractor x = new WordExtractor(new ArrayList<Document>(),new ArrayList<String>());
+		ArrayList<File> files = getFiles(directoryName);
+		for(File file : files) {
+			x.addStoppingWords(file);
+		}
+		x.printStoppingWords();
 	}
 }
